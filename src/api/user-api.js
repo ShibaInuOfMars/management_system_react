ad81646b8bc71c12bc6e0ff8bc729b721092bbd4
ShibaInuOfMars <1051858676@qq.com>
2019-09-22 23:12:16 +0800

    增加对存储在本地的用户信息的操作

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -1,6 +1,8 @@
 import ajax from './ajax';
 
-// 1. 定义一个基础路径
+import {saveObj, getObj, removeObj} from './../utils/store-tool';
+
+// 1. 定义基础路径
 const BASE_URL = 'http://localhost:3000/api/users';
 
 // 2. 登录请求
@@ -8,25 +10,31 @@ export const reqLogin = (url, {account, password}) => {
     return ajax(BASE_URL + url, {account, password}, 'post');
 };
 
-/*export const isLogin = (key = USERKEY) => {
-    if (store.get(key) === '{}') { //没有登录
+// 基础key
+const USERKEY = 'USERKEY';
+
+// 3. 判断是否登录
+export const isLogin = (key = USERKEY) => {
+    /*if (getObj(key) === '{}') { // 没有登录
         return false;
-    } else {
+    } else { // 已登录
         return true;
-    }
-};*/
+    }*/
 
-/*export const getLoginInfo = (key = USERKEY) => {
-    let info = JSON.parse(store.get(key)) || {};
-    return info;
-};*/
+    return getObj(key) !== '{}';
+};
 
+// 4. 获取登录信息
+export const getLoginInfo = (key = USERKEY) => {
+    return  JSON.parse(getObj(key)) || {};
+};
 
-/*export const saveLoginInfo = (value, key = USERKEY) => {
-    store.set(key, value);
-};*/
+// 5. 保存登录信息
+export const saveLoginInfo = (value, key = USERKEY) => {
+    saveObj(key, value);
+};
 
-/*// 5. 移动登录信息
+// 5. 移除登录信息
 export const removeLoginInfo = (key = USERKEY) => {
-    store.remove(key);
-};*/
+    removeObj(key);
+};
