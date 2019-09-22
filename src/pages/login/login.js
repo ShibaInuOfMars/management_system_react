5e21f30410b768702b1d948af1451c8f938ab71e
ShibaInuOfMars <1051858676@qq.com>
2019-09-22 16:58:17 +0800

    登录界面基本搭建

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,152 @@
 import React, {Component} from 'react';
 
-export default class Login extends Component{
+// 动态粒子特效
+import Particles from 'react-particles-js';
+
+// 登录界面的样式
+import './login.less';
+
+// 图片（必须先引入才能使用）
+import loginLogo from './../../static/images/login-logo.jpg';
+
+export default class Login extends Component {
+
+    state = {
+        particlesParams: {
+            "particles": {
+                "number": {
+                    "value": 60,
+                    "density": {
+                        "enable": true,
+                        "value_area": 1000
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 5,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 30,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 200,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true,
+            "config_demo": {
+                "hide_card": false,
+                "background_color": "#b61924",
+                "background_image": "",
+                "background_position": "50% 50%",
+                "background_repeat": "no-repeat",
+                "background_size": "cover"
+            }
+        }
+    };
+
     render() {
+        let {particlesParams} = this.state;
         return (
-            <div>
-                这是登录界面
+            <div className="loginWrapper">
+                {/* 粒子特效 */}
+                <Particles params={particlesParams}/>
+                {/* 登录面板 */}
+                <div className="loginPanel">
+                    <div className="loginTop">
+                        <img src={loginLogo} alt="动感超人"/>
+                        <span>动感-刷题后台管理系统</span>
+                    </div>
+                    <div className="loginForm">
+
+                    </div>
+                </div>
             </div>
         );
     }
-}
+};
