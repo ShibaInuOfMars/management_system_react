e99ced47ca6533c4f3f8a3b670621620a2783e35
ShibaInuOfMars <1051858676@qq.com>
2019-09-22 15:36:13 +0800

    基本路由的跳转实现

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// 相关页面
+import Admin from './pages/admin/admin';
+import Login from './pages/login/login';
 
 export default class App extends Component{
     render() {
         return (
-          <div>
-              哈哈
-          </div>
+          <Router>
+              <Switch>
+                  <Route path="/login" component={Login} />
+                  {/* 根路径要放到最后 */}
+                  <Route path="/" component={Admin} />
+              </Switch>
+          </Router>
         );
     }
 }
