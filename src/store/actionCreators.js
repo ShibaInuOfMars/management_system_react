ed2f014980be3e0045602d424603e766b2476031
ShibaInuOfMars <1051858676@qq.com>
2019-09-27 23:26:50 +0800

    redux配置

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -4,6 +4,12 @@ import {questionMsg} from './../api/question-api';
 
 export const req_question_msg = () => {
     return async (dispatch) => {
-        
+        let res = await questionMsg();
+        if (res.status === 0) {
+            dispatch({
+                type: REQ_QUESTION_MSG,
+                question_msg: res.data
+            });
+        }
     };
 };
